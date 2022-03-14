13b83b2bc6d5cb0501ca06888e7de60d986fc71e
sandeepri443 <72306158+sandeepri443@users.noreply.github.com>
2022-03-14 15:14:05 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,22 @@
+//Main Var
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "access_key" {
+	default = "AKIASYNXNQWQQ2ELL6H7"
+}
+variable "secret_key" {
+	default = "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"
+}
+
+variable "vpc_cidr" {
+  default = "10.10.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.10.1.0/24"
+}
+
+
