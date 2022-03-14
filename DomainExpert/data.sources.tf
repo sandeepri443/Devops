f34eaa4d1aaab0299eb0d3a2410287279063ca85
sandeepri443 <72306158+sandeepri443@users.noreply.github.com>
2022-03-14 15:09:16 +0530

    Create data.sources.tf

---
+++ DomainExpert/data.sources.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_availability_zones" "available"{}
+
+data "aws_internet_gateway" "mgmt_vpc_igw" {
+    filter {
+      name = "attachment.vpc-id"
+      values = [var.management_vpc_id]
+    }
+  
+}
