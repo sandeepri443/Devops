30fd37adb4fbe9edcf457b5ff7778cd218830778
sandeepri443 <72306158+sandeepri443@users.noreply.github.com>
2023-06-27 23:33:20 +0530

    Create app.tf(sym link)

---
+++ tf-iam-environment/accounts/core-account/iam/1234567891011/app.tf
@@ -0,0 +1,8 @@
+user the sym link and map it with from the common repository
+tf-iam-environment/common/iam/app.tf
+
+../../common/iam/app.tf
+
+
+to do this follow the following command
+https://phoenixnap.com/kb/symbolic-link-linux
